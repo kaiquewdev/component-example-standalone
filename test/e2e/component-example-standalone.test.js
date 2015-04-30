efa3fc2c8c9fb0e113ca2dd8d87f3a7e2610645b
Kaique da Silva <kaique.developer@gmail.com>
2015-04-30 17:09:56 -0300

    added browserify flag to .jshintrc added more tests case

--- test/e2e/component-example-standalone.test.js
+++ test/e2e/component-example-standalone.test.js
@@ -5,7 +5,9 @@ module.exports = {
       .waitForElementVisible('body', 1000)
       .waitForElementVisible('.component-example-standalone', 1000)
       .assert.elementPresent('.component-example-standalone')
-      .assert.containsText('li:first-child', 'teste 1')
+      .assert.containsText('li:nth-child(1)', 'teste 1')
+      .assert.containsText('li:nth-child(2)', 'teste 2')
+      .assert.containsText('li:nth-child(3)', 'teste 3')
       .end();
   }
 };

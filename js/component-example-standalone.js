bb33a61fe730c44140d6153b00d8a57d2d2b4e19
Kaique da Silva <kaique.developer@gmail.com>
2017-02-22 10:39:59 -0300

    Included a basic interface for a promise implementation

--- js/component-example-standalone.js
+++ js/component-example-standalone.js
@@ -1,5 +1,10 @@
 "use strict";
-function appendTextHandler(input, text) {
-  return input + text;
+function appendTextHandler() {
+  return Array.prototype.slice.call(arguments,[]).join('');
 }
 exports.appendText = appendTextHandler;
+
+function promiseHandler() {
+  return {then: function (fn) {return fn({data:[]});}}
+}
+exports.promise = promiseHandler;
--- test/bdd/component-example-standalone.test.js
+++ test/bdd/component-example-standalone.test.js
@@ -3,6 +3,15 @@ var componentExampleStandAlone = require('../../js/component-example-standalone'
 
 describe('component example standalone', function () {
   it('appendText', function () {
-    componentExampleStandAlone.appendText('hello', ' world').should.be.eql('hello world');
+    componentExampleStandAlone.appendText('hello', ' world').should.be.eql('hello world')
+    componentExampleStandAlone.appendText('one',' two',' three',' four').should.be.eql('one two three four');
+    componentExampleStandAlone.appendText(['first'],['second']).should.be.eql('firstsecond')
+  });
+
+  it('time counter', function (done) {
+    componentExampleStandAlone.promise().then(function (res) {
+      res.data.should.be.eql([]);
+      done();
+    });
   });
 });

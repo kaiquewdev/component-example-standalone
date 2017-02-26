3312100ed17494667c78f09a430af7cea2c2432e
Kaique da Silva <kaique.developer@gmail.com>
2017-02-26 18:29:47 -0300

    Extracted the method from component

--- js/component-example-standalone.js
+++ js/component-example-standalone.js
@@ -11,18 +11,20 @@ exports.appendText = appendTextHandler
 function promiseHandler(highFn) {
     let that = this;
     let output = {data:[]}
+    let isBoolean = (v) => typeof(v) === BOOLEAN
+    let isObject = (v) => typeof(v) === OBJECT
     highFn = highFn || function () {}
-    if (highFn() && typeof(highFn()) === BOOLEAN) {
+    if (highFn() && isBoolean(highFn())) {
       that.success = function (fn) {
         return fn.call(that,output)
       }
     }
-    if (!highFn() && typeof(highFn()) === BOOLEAN) {
+    if (!highFn() && isBoolean(highFn())) {
       that.fail = fn => {
 	return fn.call(that,output)
       }
     }
-    if (typeof(highFn()) === OBJECT) {
+    if (isObject(highFn())) {
       that.then = fn => {
 	output.data.push(highFn())
         return fn.call(that,output)
